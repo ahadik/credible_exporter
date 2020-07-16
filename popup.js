5b547a9a2a71ca78283e025b0bcbbb6a8b67164f
Alexander Hadik <alex@alexhadik.com>
2020-07-16 10:50:43 -0700

    Initial files for v0.0.1

---
+++ popup.js
@@ -0,0 +1,110 @@
+let generateCSV = document.getElementById('generateCSV');
+
+function getLoans() {
+  const parseLoan = (loan) => {
+
+    const topRow = loan.querySelector('._24Qwagdk').children;
+    
+
+    const lender = loan.querySelector('._3oNaBUOZ._1B4_JOwH.lN_Hn_F3._26zbLcDC').innerText;
+    const interest_rate = topRow[0].querySelector('._3Laaoq7U._3kmgqabP._253Cb2Tw._92mVf2sd.lN_Hn_F3._26zbLcDC').innerText.replace(/[^0-9.%]/g, '');
+
+    const apr = topRow[0].querySelector('._2laQWIVw._3kmgqabP._181T4gJT.lN_Hn_F3._26zbLcDC').innerText.replace(/[^0-9.%]/g, '');
+    const rate_type = topRow[1].querySelector('.didNIdwB ._3Laaoq7U._253Cb2Tw._92mVf2sd.lN_Hn_F3._26zbLcDC').innerText;
+    const total_interest = topRow[2].querySelector('._3Laaoq7U._253Cb2Tw._92mVf2sd.lN_Hn_F3._26zbLcDC').innerText;
+    
+
+    let payment_during_school;
+    let payment_after_school;
+    let duration;
+
+    const scheduleText = loan.querySelector('._15WnuB0e').innerText;
+
+    if (scheduleText === 'after graduation for') {
+      const afterSchoolDetails = loan.querySelector('._3Laaoq7U.didNIdwB');
+      payment_during_school = loan.querySelector('._3Laaoq7U._1niZNOeu ._253Cb2Tw._92mVf2sd.lN_Hn_F3._26zbLcDC').innerText;
+      payment_after_school = afterSchoolDetails.querySelectorAll('._253Cb2Tw._92mVf2sd.lN_Hn_F3._26zbLcDC')[0].innerText;
+      duration = afterSchoolDetails.querySelectorAll('._253Cb2Tw._92mVf2sd.lN_Hn_F3._26zbLcDC')[1].innerText;
+    } else if (scheduleText === 'starting immediately for') {
+      payment_during_school = loan.querySelectorAll('._3Laaoq7U._3kmgqabP._253Cb2Tw._92mVf2sd.lN_Hn_F3._26zbLcDC')[1].innerText;
+      payment_after_school = loan.querySelectorAll('._3Laaoq7U._3kmgqabP._253Cb2Tw._92mVf2sd.lN_Hn_F3._26zbLcDC')[1].innerText;
+      duration = loan.querySelector('._15WnuB0e + ._253Cb2Tw._92mVf2sd.lN_Hn_F3._26zbLcDC');
+    }
+
+    return {
+      lender,
+      interest_rate,
+      apr,
+      rate_type,
+      total_interest,
+      payment_during_school,
+      payment_after_school,
+      duration
+    }
+  };
+
+  const loanCSSClass = '._12uDxgdo';
+  const getLoans = () => document.querySelectorAll(loanCSSClass);
+
+  const expanderDOMElem = document.querySelector('._3EZ1XPOO');
+  const currNumLoans = getLoans().length;
+
+  const parseAndSendLoans = () => {
+    const loansDOM = Array.prototype.slice.call(getLoans());
+    const loans = loansDOM.map(parseLoan);
+    chrome.runtime.sendMessage(loans);
+  }
+  
+  if (expanderDOMElem.innerText === 'Show 63 less relevant loan options') {
+    expanderDOMElem.click();
+    function attemptParse() {
+      if ((getLoans().length > currNumLoans) && (parseLoan(getLoans()[getLoans().length - 1]).lender)) {
+        parseAndSendLoans();
+      } else {
+        window.setTimeout(attemptParse, 100);
+      }
+    }
+    attemptParse();
+  } else {
+    parseAndSendLoans();
+  }
+}
+
+chrome.downloads.onDeterminingFilename.addListener(function(item, suggest) {
+  suggest({filename: 'credible.csv'});
+});
+
+generateCSV.onclick = function(element) {
+  generateCSV.innerText = 'One sec...';
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    new Promise((resolve) => {
+      chrome.runtime.onMessage.addListener(
+        function(request, sender, sendResponse) {
+          resolve(request);
+        }
+      );
+    }).then((loans) => {
+      generateCSV.innerText = 'Done!';
+      window.setTimeout(() => {generateCSV.innerText = 'Go!';}, 2000)
+      let element = document.createElement('a');
+      const headers = Object.keys(loans[0])
+      const headerNames = headers.map(header => `"${header.replace('_', ' ')}"`).join(',');
+      const content = loans.map((loan) => {
+        return headers.map(header => `"${loan[header]}"`).join(',');
+      }).join('\n');
+      element.setAttribute('href', 'data:text/csv;charset=utf-8,' + encodeURIComponent(headerNames + "\n" + content));
+      element.setAttribute('download', 'credible.csv');
+
+      element.style.display = 'none';
+      document.body.appendChild(element);
+
+      element.click();
+
+      document.body.removeChild(element);
+    });
+    chrome.tabs.executeScript(
+      tabs[0].id,
+      {code: '(' + getLoans + ')();'}
+    );
+  });
+};
